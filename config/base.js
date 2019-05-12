a59323d6aa4d154a2d7247e53d4406ce44373735
Cloudbeer <cloudbeer@gmail.com>
2019-05-12 17:01:07 +0800

    change config

--- config/base.js
+++ config/base.js
@@ -6,21 +6,21 @@ module.exports = {
   //   json_validator: true,
   //   authenticate: true
   // },
-  eveConfig: {
-    asProvider: true,
-    asConsumer: true,
-    downInterval: 30,
-    upInterval: 15,
-    regServer:"http://127.0.0.1:8801"
-    // regServer: {
-    //   type: 'rest', //可选 rest, tcp, zoo
-    //   host: '127.0.0.1',
-    //   port: 8801,
-    //   username: 'youku',
-    //   password: 'youku123'
-    // },
-  },
-  providers: {
+  // eveConfig: {
+  //   asProvider: true,
+  //   asConsumer: true,
+  //   downInterval: 30,
+  //   upInterval: 15,
+  //   regServer:"http://127.0.0.1:8801"
+  //   // regServer: {
+  //   //   type: 'rest', //可选 rest, tcp, zoo
+  //   //   host: '127.0.0.1',
+  //   //   port: 8801,
+  //   //   username: 'youku',
+  //   //   password: 'youku123'
+  //   // },
+  // },
+  // providers: {
 
-  }
+  // }
 }
